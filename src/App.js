40138a03e715f4dbdff7341cb9508fd5afb957f6
jtc10005 <jtc10005@gmail.com>
2018-07-04 13:51:07 -0500

    added top navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { Home } from './containers/home';
 import Index from './containers/index';
 class App extends Component {
   render() {
     return (
       <div className={'container'}>
-        <div className={'content'}>
-          <BrowserRouter basename="">
+        <BrowserRouter basename="">
+          <div className={'content'}>
+            <div className="TopBar">
+              <Link to="/">Start</Link>
+              <Link to="/Index">Index</Link>
+            </div>
             <Switch>
               {/* `component` will render when `path` matches the user's location */}
               {/* `exact` makes it so it only renders if `path` matches exactly. */}
@@ -16,9 +20,8 @@ class App extends Component {
               <Route exact path="/" component={Home} />
               <Route path="/Index" component={Index} />
             </Switch>
-          </BrowserRouter>
-          {/* <TimedText /> */}
-        </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
